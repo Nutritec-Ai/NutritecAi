2c3c15de64faa30dabc58891a654785bc3d831c0
Ojo Michael <talk2michaelojo@gmail.com>
2025-10-10 11:43:24 +0100

    last update

--- src/app/api/gemini/route.ts
+++ src/app/api/gemini/route.ts
@@ -35,7 +35,7 @@ You are required to provide a numeric risk estimation, even if based on partial 
     console.log("📤 Sending Prompt:", prompt);
 
     const res = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
